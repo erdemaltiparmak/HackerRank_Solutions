e3f684cb4df3a1b1f63e4639626b64c7722e3940
Erdem Altıparmak <erdem52ap@gmail.com>
2021-02-08 13:49:10 +0300

    we are jumping on the clouds.

---
+++ JumpingontheClouds/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace JumpingontheClouds
+{
+    class Solution 
+    {
+
+            static int jumpingOnClouds(int[] c)
+            { 
+                if(c[c.Length-1]==1)
+                {
+                    Console.WriteLine("the last value cannot be 1");
+                    return 0;
+                }
+
+                int stepCount = 0;
+                    for (int i = 0; i < c.Length; i++)
+                    {
+                        if (i + 2 < c.Length-1)
+                        {
+                            if (c[i + 2] == 1)
+                            {
+                                stepCount++;
+                            }
+                            else
+                            {
+                                stepCount++;
+                                i++;
+
+                            }
+                        }
+                        if (i  == c.Length-1)
+                        {
+                            stepCount++;
+                        }
+
+                    }
+
+                return stepCount++;
+             }
+
+
+            static void Main(string[] args) 
+            {
+                 Console.Write("Array(ex 0,1,0,0):");
+                 string str = Console.ReadLine();
+                 int[] c = Array.ConvertAll(str.Split(','), int.Parse);
+        
+                Console.WriteLine("Step Count:" + jumpingOnClouds(c));
+                Console.WriteLine("----------------------------");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("https://github.com/erdemaltiparmak");
+
+              }
+        }
+
+}
